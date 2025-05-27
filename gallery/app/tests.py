73b04ce534c1e8c02accadd4068f75bb17ad47b4
androidToLearn <116585026+androidToLearn@users.noreply.github.com>
2025-05-28 02:21:45 +0300

    Delete gallery directory

--- gallery/app/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 494cc95ff01940e6a9be0350084b1fd140572d7d
